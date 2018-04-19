dc9308f5731b1c51fe9e1f6d3445496280fabeca
MateSzoke <szokemate@yahoo.com>
2018-04-19 23:35:22 +0200

    Add project files.

---
+++ MovieTonight/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MovieTonight.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace MovieTonight.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
